8357e40163c3a07c8a07f6267d7b321db865d284
Luis Izaguirre <luis.izaguirre@sonatype.com>
2026-06-08 19:17:48 -0300

    fix: update to latest versions, fix tests

--- index.test.js
+++ index.test.js
@@ -1,9 +1,5 @@
 const { chunkArray } = require('./index');
 
-// INTENTIONAL SYNTAX ERROR for GUIDE-2677 validation failure testing
-// This will cause Jest to fail to parse the file
-const broken = {];
-
 describe('chunkArray', () => {
   it('should chunk array', () => {
     expect(chunkArray([1, 2, 3, 4], 2)).toEqual([[1, 2], [3, 4]]);
