71ac34803fc11ca03e357a942eaed99d7b976e92
Luis Izaguirre <luis.izaguirre@sonatype.com>
2026-06-09 16:49:37 -0300

    Restore passing tests

--- index.test.js
+++ index.test.js
@@ -4,9 +4,4 @@ describe('chunkArray', () => {
   it('should chunk array', () => {
     expect(chunkArray([1, 2, 3, 4], 2)).toEqual([[1, 2], [3, 4]]);
   });
-
-  it('should fail intentionally to test AGP validation', () => {
-    // This test will always fail
-    expect(true).toBe(false);
-  });
 });
